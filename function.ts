a957804da821697165ad8560c02a2e6569682587
Mateus Sanches Rodriguez <mateussanchesrodriguez@gmail.com>
2023-05-29 23:53:34 -0300

    primeiro commit

---
+++ function.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number) {
+    return a * b;
+}
+
+function saudacao(nome: string) {
+    return console.log(`Olá ${nome}`)
+    
+}
+
+saudacao("Mateus")
+console.log(multiplicacao(10,20))
